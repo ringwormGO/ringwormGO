815266f1ed549245c20c705980bc955d93b82186
Andrej123456789 <83548580+Andrej123456789@users.noreply.github.com>
2021-08-09 11:36:00 +0200

    Add files via upload

---
+++ Hangman/Console/About.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Console type" << endl <<  "Credits: YouTube tutorials" << endl << "Publisher and upgrader: ringwormGO" << endl;
+    system("pause");
+    return 0;
+}
