498ba689301b1019860cbc7ad7aa7d19d5eaa452
Andrej123456789 <83548580+Andrej123456789@users.noreply.github.com>
2021-09-02 15:55:40 +0200

    Unity scripts (client)
    Just for private or for ringwormGO use.

--- 3D PC SANDBOX game (Unity)/Game server (beta)/Client.cs
+++ 3D PC SANDBOX game (Unity)/Game server (beta)/Client.cs
@@ -1,99 +1,153 @@
-﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
 using System.Net;
 using System.Net.Sockets;
+using System;
 
-namespace GameServer
+public class Client : MonoBehaviour
 {
-    class Client
+    public Text m_text;
+
+    public static Client instance;
+    public static int dataBufferSize = 4096;
+
+    public string ip = "192.168.1.6";
+    public int port = 26950;
+    public int myId = 0;
+    public TCP tcp;
+
+    private delegate void PacketHandler(Packet _packet);
+    private static Dictionary<int, PacketHandler> packetHandlers;
+
+    private void Awake()
     {
-        public static int dataBufferSize = 4096;
-
-        public int id;
-        public TCP tcp;
-
-        public Client(int _clientId)
+        if (instance == null)
         {
-            id = _clientId;
-            tcp = new TCP(id);
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        tcp = new TCP();
+    }
+
+    public void ConnectToServer()
+    {
+        InitializeClientData();
+
+        tcp.Connect();
+    }
+
+    public class TCP
+    {
+        public TcpClient socket;
+
+        private NetworkStream stream;
+        private Packet receivedData;
+        private byte[] receiveBuffer;
+
+        public void Connect()
+        {
+            socket = new TcpClient
+            {
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+            receiveBuffer = new byte[dataBufferSize];
+            socket.BeginConnect(instance.ip, instance.port, ConnectCallback, socket);
         }
 
-        public class TCP
+        private void ConnectCallback(IAsyncResult _result)
         {
-            public TcpClient socket;
+            socket.EndConnect(_result);
 
-            private readonly int id;
-            private NetworkStream stream;
-            private Packet receivedData;
-            private byte[] receiveBuffer;
-
-            public TCP(int _id)
+            if (!socket.Connected)
             {
-                id = _id;
+                return;
             }
 
-            public void Connect(TcpClient _socket)
+            stream = socket.GetStream();
+
+            receivedData = new Packet();
+
+            stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }
+
+        public void SendData(Packet _packet)
+        {
+            try
             {
-                socket = _socket;
-                socket.ReceiveBufferSize = dataBufferSize;
-                socket.SendBufferSize = dataBufferSize;
-
-                stream = socket.GetStream();
-
-                receivedData = new Packet();
-                receiveBuffer = new byte[dataBufferSize];
-
-                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
-
-                ServerSend.Welcome(id, "Welcome to the server!");
-            }
-
-            public void SendData(Packet _packet)
-            {
-                try
+                if (socket != null)
                 {
-                    if (socket != null)
-                    {
-                        stream.BeginWrite(_packet.ToArray(), 0, _packet.Length(), null, null);
-                    }
-                }
-                catch (Exception _ex)
-                {
-                    Console.WriteLine($"Error sending data to player {id} via TCP: {_ex}");
+                    stream.BeginWrite(_packet.ToArray(), 0, _packet.Length(), null, null);
                 }
             }
-
-            private void ReceiveCallback(IAsyncResult _result)
+            catch (Exception _ex)
             {
-                try
-                {
-                    int _byteLength = stream.EndRead(_result);
-                    if (_byteLength <= 0)
-                    {
-                        // TODO: disconnect
-                        return;
-                    }
+                Debug.Log($"Error sending data to server via TCP: {_ex}");
+            }
+        }
 
-                    byte[] _data = new byte[_byteLength];
-                    Array.Copy(receiveBuffer, _data, _byteLength);
-
-                    receivedData.Reset(HandleData(_data));
-                    stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
-                }
-                catch (Exception _ex)
+        private void ReceiveCallback(IAsyncResult _result)
+        {
+            try
+            {
+                int _byteLength = stream.EndRead(_result);
+                if (_byteLength <= 0)
                 {
-                    Console.WriteLine($"Error receiving TCP data: {_ex}");
                     // TODO: disconnect
+                    return;
+                }
+
+                byte[] _data = new byte[_byteLength];
+                Array.Copy(receiveBuffer, _data, _byteLength);
+
+                receivedData.Reset(HandleData(_data));
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            }
+            catch
+            {
+                // TODO: disconnect
+            }
+        }
+
+        private bool HandleData(byte[] _data)
+        {
+            int _packetLength = 0;
+
+            receivedData.SetBytes(_data);
+
+            if (receivedData.UnreadLength() >= 4)
+            {
+                _packetLength = receivedData.ReadInt();
+                if (_packetLength <= 0)
+                {
+                    return true;
                 }
             }
 
-            private bool HandleData(byte[] _data)
+            while (_packetLength > 0 && _packetLength <= receivedData.UnreadLength())
             {
-                int _packetLength = 0;
+                byte[] _packetBytes = receivedData.ReadBytes(_packetLength);
+                ThreadManager.ExecuteOnMainThread(() =>
+                {
+                    using (Packet _packet = new Packet(_packetBytes))
+                    {
+                        int _packetId = _packet.ReadInt();
+                        packetHandlers[_packetId](_packet);
+                    }
+                });
 
-                receivedData.SetBytes(_data);
-
+                _packetLength = 0;
                 if (receivedData.UnreadLength() >= 4)
                 {
                     _packetLength = receivedData.ReadInt();
@@ -102,37 +156,24 @@ namespace GameServer
                         return true;
                     }
                 }
-
-                while (_packetLength > 0 && _packetLength <= receivedData.UnreadLength())
-                {
-                    byte[] _packetBytes = receivedData.ReadBytes(_packetLength);
-                    ThreadManager.ExecuteOnMainThread(() =>
-                    {
-                        using (Packet _packet = new Packet(_packetBytes))
-                        {
-                            int _packetId = _packet.ReadInt();
-                            Server.packetHandlers[_packetId](id, _packet);
-                        }
-                    });
-
-                    _packetLength = 0;
-                    if (receivedData.UnreadLength() >= 4)
-                    {
-                        _packetLength = receivedData.ReadInt();
-                        if (_packetLength <= 0)
-                        {
-                            return true;
-                        }
-                    }
-                }
-
-                if (_packetLength <= 1)
-                {
-                    return true;
-                }
-
-                return false;
             }
+
+            if (_packetLength <= 1)
+            {
+                return true;
+            }
+
+            return false;
         }
+    }
+
+    private void InitializeClientData()
+    {
+        packetHandlers = new Dictionary<int, PacketHandler>()
+        {
+            { (int)ServerPackets.welcome, ClientHandle.Welcome }
+        };
+        Debug.Log("Initialized packets.");
+        //m_text.text = string.Format("Initialized packets");
     }
 }
