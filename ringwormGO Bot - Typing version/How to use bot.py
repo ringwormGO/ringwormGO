892f6cd7b1e649e041c90e2ba880044f651cdc6b
Andrej123456789 <83548580+Andrej123456789@users.noreply.github.com>
2021-09-30 15:11:12 +0200

    Delete ringwormGO Bot - Typing version directory

--- ringwormGO Bot - Typing version/How to use bot.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-print("Keep utils folder and some other files in one folder!!")
-print("All update will perform on this files!! In v2.0.0 all coming in one file.")
-print("Navigate folder to Command Prompt or Terminal and type python [name of file]")
-
-os.system('pause')
